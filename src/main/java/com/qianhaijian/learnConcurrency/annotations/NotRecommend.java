7627a86f89c27e7b68fd5ddb178fe0b446c7cac0
greatqian <qdaxia666@gmail.com>
2019-04-08 15:10:50 +0800

    add annotaions

---
+++ src/main/java/com/qianhaijian/learnConcurrency/annotations/NotRecommend.java
@@ -0,0 +1,20 @@
+package com.qianhaijian.learnConcurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by 前路漫漫还相见 on 2019/4/8.
+ */
+/*
+    标记不推荐写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+
+}
